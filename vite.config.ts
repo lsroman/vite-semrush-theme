c5845423ad40fcaa62d128f6c8141273e2226eba
Oleh Kutsenko <inthedark1225@gmail.com>
2022-12-06 09:07:14 +0800

    add global theme overrides for the primary button

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
+import path from "node:path";
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react({
+    babel: {
+      plugins: [
+        [
+          '@semcore/babel-plugin-react-semcore',
+          { 
+            theme: path.resolve(process.cwd(), 'src', 'theme'),
+            verbose: true
+          }
+        ]
+      ]
+    }
+  })]
 })
