a2e3ec28267cfa79610f8925df5f2f355e8b9e5d
Oleh Kutsenko <inthedark1225@gmail.com>
2022-12-06 08:57:39 +0800

    add semcore packages. demo buttons in the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import Button from "@semcore/button";
+import { Flex } from "@semcore/flex-box";
+
 function App() {
-  return <div>Hello</div>;
+  return (
+    <Flex direction="column" gap={4} alignItems="flex-start">
+      <Button use="primary">Primary Button</Button>
+      <Button use="secondary">Secondary Button</Button>
+    </Flex>
+  );
 }
 
 export default App;
