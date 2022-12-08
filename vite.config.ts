73707491471d20f9a5fadb2a6236faefd66a8901
r.lysov <r.lysov@semrush.com>
2022-12-08 17:38:25 +0200

    Add vite-plugin-babel

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,20 @@
 import { defineConfig } from 'vite'
 import path from "node:path";
 import react from '@vitejs/plugin-react'
+import babel from 'vite-plugin-babel'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react({
-    babel: {
-      plugins: [
-        [
-          '@semcore/babel-plugin-react-semcore',
-          { 
+  plugins: [react(),
+    babel({
+      babelConfig: {
+        plugins: [
+          ["@semcore/babel-plugin-react-semcore", {
             theme: path.resolve(process.cwd(), 'src', 'theme'),
             verbose: true
-          }
+          }]
         ]
-      ]
-    }
-  })]
+      },
+    })
+  ]
 })
